8020f5d446b588fb2941b5f7aad0012dfd1f9629
aalvarez <alvarez.vargasgmail.com>
2020-07-30 12:46:17 -0400

    Se elimina autorizacion via json web token para listar materias

--- routes/materias.js
+++ routes/materias.js
@@ -5,7 +5,7 @@ const auth = require('../middleware/auth');
 const {listarMaterias, crearMateria, actualizarMaterias, eliminarMaterias, datosMaterias} = require('../controllers/materiaController');
 
 router.post('/crear',auth, crearMateria);
-router.get('/listar', auth, listarMaterias);
+router.get('/listar', listarMaterias);
 router.put('/actualizar', auth, actualizarMaterias);
 router.delete('/eliminar/:codigo', auth, eliminarMaterias);
 router.get('/datos/:codigo', auth, datosMaterias);
