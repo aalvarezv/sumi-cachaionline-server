eea208105e3997f5eefc29348dabd9e96ea6637d
aalvarez <alvarez.vargasgmail.com>
2021-06-16 21:59:33 -0400

    Se agregan campos createdAt y updatedAt en migracion que crea la tabla usuario_recupera_clave

--- database/migrations/20210610204302-create-table-usuario-recupera-clave.js
+++ database/migrations/20210610204302-create-table-usuario-recupera-clave.js
@@ -1,20 +1,32 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+
     await queryInterface.createTable('usuario_recupera_claves', {
       rut_usuario: {
-        type: Sequelize.STRING(12),
-        primaryKey: true,
-        allowNull: false,
-        references: {
-          model: 'usuarios',
-          key: 'rut'
-        }
+          type: Sequelize.STRING(12),
+          primaryKey: true,
+          allowNull: false,
+          references: {
+            model: 'usuarios',
+            key: 'rut'
+          }
       },
       codigo_recupera_clave: {
-        type: Sequelize.STRING(128),
-        allowNull: false,
+          type: Sequelize.STRING(128),
+          allowNull: false,
+      },
+      createdAt: {
+          type: Sequelize.DataTypes.DATE,
+          allowNull: false,
+          defaltValue: Sequelize.fn('NOW'),
+      }, 
+      updatedAt: {
+          type: Sequelize.DataTypes.DATE,
+          allowNull: false,
+          defaltValue: Sequelize.fn('NOW'),
       }
+
     });
   },
   down: async (queryInterface, Sequelize) => {
